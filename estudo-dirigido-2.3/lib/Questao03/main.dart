2b3bd009f88c02a386a61a89cf3b2bf4163811b1
leonardo9821 <leonardo25teixeira@gmailo.com>
2026-05-18 21:35:08 -0300

    mudei

--- estudo-dirigido-2.3/lib/Questao03/main.dart
+++ estudo-dirigido-2.3/lib/Questao03/main.dart
@@ -1,3 +1,4 @@
+import '../../../Estudo-dirigido-2.1/lib/Questao03/veiculo.dart';
 import 'carro.dart';
 import 'moto.dart';
 import 'caminhao.dart';
@@ -5,22 +6,28 @@ import 'Fichavel.dart';
 import 'listaGenerica.dart';
 
 void main() {
-  var carro01 = Carro('Toyota', 'Corolla', 2022, 'Carro', '4 portas');
-  var carro02 = Carro('Honda', 'Civic', 2021, 'Carro', '4 portas');
-  var moto01 = Moto('Yamaha', 'Fazer', 2020, 'Moto', '250 Cilindradas');
-  var moto02 = Moto('Honda', 'Biz', 2020, 'Moto', '125 Cilindraqdas ');
-  var caminhao = Caminhao('Volvo', 'FH', 2019, 'Caminhão ', '12 Toneladas');
+  Carro carro01 = Carro('Toyota', 'Corolla', 2022, 'Carro', '4 portas');
+  Carro carro02 = Carro('Honda', 'Civic', 2021, 'Carro', '4 portas');
+  Moto moto01 = Moto('Yamaha', 'Fazer', 2020, 'Moto', '250 Cilindradas');
+  Moto moto02 = Moto('Honda', 'Biz', 2020, 'Moto', '125 Cilindraqdas ');
+  Caminhao caminhao = Caminhao(
+    'Volvo',
+    'FH',
+    2019,
+    'Caminhão ',
+    '12 Toneladas',
+  );
 
-  var veiculo = ListaGenerica<Fichavel>();
+  ListaGenerica veiculo = ListaGenerica<Fichavel>();
   veiculo.adicionar(carro01);
   veiculo.adicionar(carro02);
   veiculo.adicionar(moto01);
   veiculo.adicionar(moto02);
   veiculo.adicionar(caminhao);
 
-  //veiculo.imprimirItens(); -> porque precisso colocar ele se vai repetir duas vezes 
+  //veiculo.imprimirItens(); -> porque precisso colocar ele se vai repetir duas vezes
 
   for (var veiculo in veiculo.itens) {
-    veiculo.exibirFicha();
+    print(veiculo);
   }
 }
